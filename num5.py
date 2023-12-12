3c26d25b26a62dbd4830f84d1951d6e136e21dc7
LYNAYLUKE1027 <67407640+LYNAYLUKE1027@users.noreply.github.com>
2023-12-13 01:07:14 +0900

    Add files via upload

---
+++ num5.py
@@ -0,0 +1,12 @@
+def solution(numbers) :
+    answer = ''
+
+    for i in range(len(numbers)) :
+        num[i] = numbers[i]
+        print(num[i])
+
+
+    return answer
+
+numbers = [6, 10, 2]
+solution(numbers)
